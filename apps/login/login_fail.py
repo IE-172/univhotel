9c53cabb32fdf397d6a593e608e48190638613c6
blt-ang <btang@up.edu.ph>
2024-01-21 17:47:04 +0800

    Update login_fail.py

--- apps/login/login_fail.py
+++ apps/login/login_fail.py
@@ -19,7 +19,7 @@ from apps import dbconnect as db
 
 layout = html.Div(
     [
-        html.Div([html.H2('Staff Credentials', style={'color': '#8A1538'})], style = {'float':'left'}),
+        html.Div([html.H2('Staff Credentials', style={'color': '#8A1538','margin-top': '15px'})], style = {'margin-left':'40px','font-weight':'bold','float':'left'}),
         html.Br(),
         html.Br(),
         html.Br(),
@@ -27,13 +27,20 @@ layout = html.Div(
             [
                 dbc.CardBody(
                     [
-                        html.H4("You do not have access to this page"),
-                        html.P("Please contact an admin for assistance.")
+                        html.H4("You do not have access to this page", style = {'margin-left':'40px'}),
+                        html.P("Please contact an admin for assistance.", style = {'margin-left':'40px'})
                         
-                    ]
+                    ], 
+                style={'background-color':'#ffffff','margin-left':'20px','margin-right':'20px'}
                 )
-            ]
-        ),
-    ]
+            ],
+        style={'background-color': 'transparent'}
+        )
+    ],
+        style={
+        'background-image': 'url("assets/backgroundimage.png")',
+        'height': '100vh',
+        'width': '100%',
+        'margin': '0'}
 )
 
